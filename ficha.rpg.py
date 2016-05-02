138f21bb80d071eb6a6d8c5233b35a292b65c3ba
Leonardo Alcântara <leogif@gmail.com>
2016-05-02 05:03:25 -0300

    Ficha RPG

---
+++ ficha.rpg.py
@@ -0,0 +1,13 @@
+# PONTO DE HABILIDADE DO HEROI
+hab = int(input("Informe seus pontos de habilidade "))
+phh = hab + 6
+# PONTO DE ENERGIA DO HEROI
+ene = int(input("Informe seus pontos de energia "))
+peh = ene + 12
+# PONTO DE SORTE DO HEROI
+sor = int(input("Informe seus pontos de sorte "))
+psh = sor + 6
+print ("Seus pontos de Habilidades sao: ",phh)
+print ("Seus pontos de Energia sao: ",peh)
+print ("Seus pontos de Sorte sao: ",psh)
+itensequipamentoslevados = input [" "]
